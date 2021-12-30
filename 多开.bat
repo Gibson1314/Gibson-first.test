0e59b0ef96676c58d75d172594fa67bf3a9f2bba
Gibson1314 <96857923+Gibson1314@users.noreply.github.com>
2021-12-30 16:32:16 +0800

    Update 多开.bat

--- 多开.bat
+++ 多开.bat
@@ -1,3 +1,6 @@
 start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
 start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
 start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
+start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
+start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
+start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
